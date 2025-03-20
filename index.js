2fe0d38781c8059fc5af11ff3716e443bf393d4f
raiardia <ardiaanggreni@gmail.com>
2025-03-20 13:01:20 +0800

    2. HTTP Server Sederhana dengan node:http

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello, world!'); 
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}); 
+
+server.listen(3000, () => console.log('Server is running on http://localhost:3000'));
