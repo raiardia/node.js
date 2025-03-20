bc42a1bce58167fb139835a69d0d6669636867d5
raiardia <ardiaanggreni@gmail.com>
2025-03-20 13:38:26 +0800

    3. Koneksi Database MySQL pada Node.js

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-import http from 'http';
+import http from "http";
+import mysql from "mysql2";
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "openApi",
+  password: "",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err.stack);
+    return;
+  }
+  console.log("Connected to database");
+});
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}); 
+  
+  db.query("SELECT * FROM users", (err, result) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ error: "Internal server error" }));
+      return;
+    }
 
-server.listen(3000, () => console.log('Server is running on http://localhost:3000'));
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(result));
+  });
+});
+
+server.listen(3000, () =>
+  console.log("Server is running on http://localhost:3000")
+);
